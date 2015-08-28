6c59fca1972846dfd4702ef518847a882a74afc2
Russ Olsen <russ@russolsen.com>
2015-08-28 15:36:04 -0400

    Added specs.

--- spec/expletive_spec.cr
+++ spec/expletive_spec.cr
@@ -1,9 +1,73 @@
 require "./spec_helper"
 
 describe Expletive do
-  # TODO: Write tests
+  describe Expletive::Dump do
+    it "passes text unchanged" do
+      input = "hello world"
+      input.should eq(endump(input))
+    end
 
-  it "works" do
-    false.should eq(true)
+    it "passes empty strings unchanged" do
+      input = ""
+      input.should eq(endump(input))
+    end
+
+    it "encodes binary into hex" do
+      input = "\01\02\03xyz"
+      "\\01\\02\\03xyz".should eq(endump(input))
+    end
+
+    it "escapes backslashes" do
+      input = "abc\\def"
+      "abc\\\\def".should eq(endump(input))
+    end
+
+    it "escapes newlines" do
+      input = "abc\ndef"
+      "abc\\ndef".should eq(endump(input))
+    end
+  end
+
+  describe Expletive::Undump do
+    describe "#run" do
+      it "passes text unchanged" do
+        input = "hello world"
+        input.should eq(dedump(input))
+      end
+
+      it "leaves empty strings as is" do
+        input = ""
+        input.should eq(dedump(input))
+      end
+
+
+      it "decodes hex into equivalent binary" do
+        input = "\\01\\02\\03xyz"
+        output = "\01\02\03xyz"
+        output.should eq(dedump(input))
+      end
+
+      it "turns backslash backslash into a single backslash" do
+        input = "abc\\\\def"
+        output = "abc\\def"
+        output.should eq(dedump(input))
+      end
+
+      it "turns backslash n into a real newline" do
+        input = "abc\\ndef"
+        output = "abc\ndef"
+        output.should eq(dedump(input))
+      end
+    end
+  end
+
+  describe "RoundTrip" do
+    it "converts conveys random strings without changing them" do
+      5000.times do
+        input = random_string(300)
+        output = round_trip(input)
+        input.should eq(output)
+      end
+    end
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,35 @@
 require "spec"
 require "../src/expletive"
+
+SRAND = Random.new(3478)
+
+def random_string(max_len=10)
+  l = SRAND.next_int % 255
+
+  bytes = [] of UInt8
+  l.times do
+    ch = (SRAND.next_int % 255).to_u8
+    bytes << ch
+  end
+  sio = StringIO.new
+  sio.write(bytes)
+  sio.to_s
+end
+
+def endump(input)
+  in_io = StringIO.new(input)
+  out_io = StringIO.new
+  Expletive::Dump.new(in_io, out_io).run
+  out_io.to_s
+end
+
+def dedump(input)
+  in_io = StringIO.new(input)
+  out_io = StringIO.new
+  Expletive::Undump.new(in_io, out_io).run
+  out_io.to_s
+end
+
+def round_trip(input)
+  dedump(endump(input))
+end
